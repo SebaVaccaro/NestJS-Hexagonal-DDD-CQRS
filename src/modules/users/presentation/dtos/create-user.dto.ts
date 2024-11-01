3080ebabfe797ce89f69cfbf41410c9acd60f384
SebaVaccaro <soy.vaccaro@gmail.com>
2024-11-01 08:13:05 -0300

    add DTOS

---
+++ src/modules/users/presentation/dtos/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUserDto{
+    @IsString()
+    @IsNotEmpty()
+    readonly userId: string
+    @IsString()
+    @IsNotEmpty()
+    readonly username: string
+    @IsEmail()
+    @IsNotEmpty()
+    readonly email: string
+    @IsString()
+    @IsNotEmpty()
+    readonly password: string
+}
