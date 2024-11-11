c67ba2ba18d6c8ea9237287bec7ff98087ecdb4a
SebaVaccaro <soy.vaccaro@gmail.com>
2024-11-10 22:37:54 -0300

    configure cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+})
   app.use(cookieParser())
   await app.listen(3000, ()=>console.log("listen on port 3000"));
 }
